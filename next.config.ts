29a5ca07d954dadee9b99e716645be4980aa887e
esnir74 <esnir74@gmail.com>
2025-10-13 17:31:01 +0900

    外部の画像貼れるように（一時的に）

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // 任意の外部ドメインを許可する場合
+      },
+    ],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
